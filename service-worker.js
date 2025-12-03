318237d10d18f785249376665b1f378380126f34
Egldmoney <beontop.belgium@gmail.com>
2025-12-03 12:16:28 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// service-worker.js
+const CACHE_NAME = 'dirty-cleaning-calculator-v1';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './cleaning-calculator.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
